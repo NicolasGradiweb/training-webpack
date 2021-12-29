8ee33b50f562d1a4bcc343075f615e0a6f9209aa
Nicolas Mosquera <nicolas.mosquera@gradiweb.com>
2021-12-28 21:53:54 -0500

    Make changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import Template from '@templates/Template.js';
 import '@styles/main.css';
 import '@styles/variables.scss'
 
-console.log('hello')
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
